862b717f52cc8e501f3b3c0ea946f052a8262509
Dawid Marcinkowski <marcinkowski.dawidd@gmail.com>
2022-09-13 10:37:32 +0200

    Front app setup with webpack

---
+++ client/src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+
+// Components
+import { App } from './components';
+
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
---
+++ client/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.tsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+}
